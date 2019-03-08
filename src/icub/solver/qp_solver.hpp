5802d1e47287f4f2ebb9e4672cee0143bbc767e4
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2019-03-08 16:59:11 +0100

    Working on qp solver

--- src/icub/solver/qp_solver.hpp
+++ src/icub/solver/qp_solver.hpp
@@ -3,15 +3,24 @@
 
 #include <qpOASES.hpp>
 
+#include <icub/model/iCub.hpp>
+
 namespace icub {
     namespace solver {
         template <typename QPProblem>
         class QPSolver {
         public:
             QPSolver() {}
+            QPSolver(model::iCub* icub) : _icub(icub) {}
+
+            void set_icub(model::iCub* icub)
+            {
+                _icub = icub;
+            }
 
         protected:
             QPProblem _solver;
+            model::iCub* _icub;
         };
     } // namespace solver
 } // namespace icub
