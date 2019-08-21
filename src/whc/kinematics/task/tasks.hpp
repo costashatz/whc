a835d6c858ea004ec89d2f26c351facb9a25165c
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2019-08-21 11:57:51 +0200

    Added license

--- src/whc/kinematics/task/tasks.hpp
+++ src/whc/kinematics/task/tasks.hpp
@@ -1,3 +1,21 @@
+//|
+//|    Copyright (C) 2019 Learning Algorithms and Systems Laboratory, EPFL, Switzerland
+//|    Author/Maintainer:  Konstantinos Chatzilygeroudis
+//|    email:   konstantinos.chatzilygeroudis@epfl.ch
+//|    website: lasa.epfl.ch
+//|
+//|    This file is part of whc.
+//|
+//|    whc is free software: you can redistribute it and/or modify
+//|    it under the terms of the GNU General Public License as published by
+//|    the Free Software Foundation, either version 3 of the License, or
+//|    (at your option) any later version.
+//|
+//|    whc is distributed in the hope that it will be useful,
+//|    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//|    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//|    GNU General Public License for more details.
+//|
 #ifndef WHC_KINEMATICS_TASK_TASKS_HPP
 #define WHC_KINEMATICS_TASK_TASKS_HPP
 
